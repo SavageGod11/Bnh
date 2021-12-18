a624bf4103132064dcf88f913956a5bb3a6fef8e
SavageGod11 <88664848+SavageGod11@users.noreply.github.com>
2021-12-18 20:10:41 +0200

    Update Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
         if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term : *${term}*`)
         const audio = new YT(videos[0].url, 'audio')
         if (!audio.url) return
-        M.reply('「 WAIT...」')
+        M.reply('*🎶 Downloading Your Song...*')
         this.client
             .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
                 quoted: M.WAMessage,
