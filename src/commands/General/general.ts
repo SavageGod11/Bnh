457556db275a844172d9bba10cf5b2bc03db971f
SavageGod11 <88664848+SavageGod11@users.noreply.github.com>
2021-12-19 22:17:02 +0200

    Update general.ts

--- src/commands/General/general.ts
+++ src/commands/General/general.ts
@@ -25,7 +25,7 @@ export default class Command extends BaseCommand {
         const mn=`
 Greetings ${M.sender.username}
     You requested ${this.client.config.prefix}general commands
- ```👾Available Commands👾```
+ \`\`\`👾Available Commands👾\`\`\`
    
         *━━━━❰ General ❱━━━━*
  *- ${this.client.config.prefix}admin*
